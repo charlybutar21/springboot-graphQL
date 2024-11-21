84b0cb6b4d7ef9b638cc7598d1948f0aad631abf
Charly Micolas Butarbutar <charlymicolasbutarbutar@pinjamwinwin.com>
2024-11-21 10:47:55 +0700

    feat: add resolver error execption

---
+++ src/main/java/com/charly/springbootgraphql/config/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.charly.springbootgraphql.config;
+
+import com.charly.springbootgraphql.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", "Resource Not Found");
+        response.put("message", ex.getMessage());
+        response.put("status", HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
